dc8bcd0b9a796eb746fee772a00031b294283b3e
Daniel Ranner <daniel.ranner@jollydays.com>
2023-05-08 21:06:04 +0200

    Added failing test and test run configs

---
+++ src/test/java/ninja/ranner/tictactoe/GameTest.java
@@ -0,0 +1,12 @@
+package ninja.ranner.tictactoe;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void yeet() throws Exception {
+        throw new UnsupportedOperationException("yeet is not implemented.");
+    }
+
+}
--- src/test/java/ninja/ranner/tictactoe/TicTacToeApplicationTests.java
+++ src/test/java/ninja/ranner/tictactoe/TicTacToeApplicationTests.java
@@ -1,9 +1,11 @@
 package ninja.ranner.tictactoe;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("spring")
 class TicTacToeApplicationTests {
 
 	@Test
