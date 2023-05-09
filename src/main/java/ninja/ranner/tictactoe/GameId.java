14a22a7a59c307e27b714a2aec6da6946e229d36
Daniel Ranner <daniel.ranner@jollydays.com>
2023-05-09 23:04:49 +0200

    F created GameId record

---
+++ src/main/java/ninja/ranner/tictactoe/GameId.java
@@ -0,0 +1,13 @@
+package ninja.ranner.tictactoe;
+
+import java.util.UUID;
+
+public record GameId(UUID value) {
+  public static GameId of(UUID value) {
+    return new GameId(value);
+  }
+
+  public static GameId create() {
+    return new GameId(UUID.randomUUID());
+  }
+}
