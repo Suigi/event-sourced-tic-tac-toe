76bbbc22c7b371ade9ca0001d461d19f4c97dd95
Daniel Ranner <daniel.ranner@jollydays.com>
2023-05-09 23:07:08 +0200

    F created constructors for GameCreated event

--- src/main/java/ninja/ranner/tictactoe/GameCreated.java
+++ src/main/java/ninja/ranner/tictactoe/GameCreated.java
@@ -1,4 +1,14 @@
 package ninja.ranner.tictactoe;
 
+import java.util.UUID;
+
 public record GameCreated(java.util.UUID gameId) implements Event {
+
+  public GameCreated(GameId gameId) {
+    this(gameId.value());
+  }
+
+  public GameCreated(UUID gameId) {
+    this.gameId = gameId;
+  }
 }
