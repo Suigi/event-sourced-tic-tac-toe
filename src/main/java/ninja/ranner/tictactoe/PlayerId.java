0fa60714502c472360b4e37ccf44f87314a6c303
Daniel Ranner <daniel.ranner@jollydays.com>
2023-05-09 23:13:26 +0200

    F created PlayerId record

---
+++ src/main/java/ninja/ranner/tictactoe/PlayerId.java
@@ -0,0 +1,13 @@
+package ninja.ranner.tictactoe;
+
+import java.util.UUID;
+
+public record PlayerId(UUID value) {
+  public static PlayerId of(UUID value) {
+    return new PlayerId(value);
+  }
+
+  public static PlayerId create() {
+    return new PlayerId(UUID.randomUUID());
+  }
+}
