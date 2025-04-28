8a8fce0656a7d8c4fd6bcba46502dd816a56d775
Daniel Ranner <6762000+Suigi@users.noreply.github.com>
2025-04-28 08:46:21 +0200

    Removed obsolete hard-coded game

--- src/main/java/ninja/ranner/xogame/spring/XoGameConfiguration.java
+++ src/main/java/ninja/ranner/xogame/spring/XoGameConfiguration.java
@@ -4,14 +4,9 @@ import ninja.ranner.xogame.application.port.EventStore;
 import ninja.ranner.xogame.application.port.GameIdGenerator;
 import ninja.ranner.xogame.application.port.GameRepository;
 import ninja.ranner.xogame.application.port.InMemoryEventStore;
-import ninja.ranner.xogame.domain.Cell;
-import ninja.ranner.xogame.domain.Game;
 import ninja.ranner.xogame.domain.GameId;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.UUID;
 
 @Configuration
 public class XoGameConfiguration {
@@ -30,18 +25,5 @@ public class XoGameConfiguration {
     GameIdGenerator gameIdGenerator() {
         return GameId::random;
     }
-
-    @Bean
-    ApplicationRunner initializeGame(GameRepository gameRepository) {
-        return _ -> {
-            Game game = Game.create(
-                    GameId.of(UUID.fromString("00000000-0000-0000-0000-000000000000")),
-                    "Hard-coded Game");
-            game.fillCell(Cell.at(0, 0));
-            game.fillCell(Cell.at(1, 1));
-            gameRepository.save(game);
-        };
-    }
-
 
 }
