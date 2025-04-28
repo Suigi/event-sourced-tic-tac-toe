5b713b99418113c5340788c9e4a03a9262317bb9
Daniel Ranner <6762000+Suigi@users.noreply.github.com>
2025-04-28 08:46:21 +0200

    Run config for fast tests

--- src/test/java/ninja/ranner/xogame/XoGameApplicationTests.java
+++ src/test/java/ninja/ranner/xogame/XoGameApplicationTests.java
@@ -1,11 +1,13 @@
 package ninja.ranner.xogame;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@Tag("spring")
 class XoGameApplicationTests {
 
     @Test
