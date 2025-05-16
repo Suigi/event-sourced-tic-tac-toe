087d48100d16eae99395518ac4eb8afaab1347f7
Daniel Ranner <6762000+Suigi@users.noreply.github.com>
2025-05-16 18:23:58 +0200

    Using JdbcEventStore when running the application

--- src/main/java/ninja/ranner/xogame/spring/XoGameConfiguration.java
+++ src/main/java/ninja/ranner/xogame/spring/XoGameConfiguration.java
@@ -1,20 +1,27 @@
 package ninja.ranner.xogame.spring;
 
+import ninja.ranner.xogame.adapter.out.jdbc.GameEventNameMapper;
+import ninja.ranner.xogame.adapter.out.jdbc.JdbcEventStore;
 import ninja.ranner.xogame.application.GameService;
 import ninja.ranner.xogame.application.port.EventStore;
 import ninja.ranner.xogame.application.port.GameIdGenerator;
 import ninja.ranner.xogame.application.port.GameRepository;
-import ninja.ranner.xogame.application.port.InMemoryEventStore;
 import ninja.ranner.xogame.domain.GameId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration(proxyBeanMethods = false)
 public class XoGameConfiguration {
 
     @Bean
-    EventStore eventStore() {
-        return new InMemoryEventStore();
+    EventStore eventStore(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        return new JdbcEventStore(
+                jdbcTemplate,
+                namedParameterJdbcTemplate,
+                new GameEventNameMapper()
+        );
     }
 
     @Bean
